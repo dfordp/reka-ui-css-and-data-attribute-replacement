6a5f10a8bbdba4517ae91ffa100e633409e184af
Dilpreet <92905896+dfordp@users.noreply.github.com>
2024-10-29 20:50:27 +0530

    still no fixes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import type { Api } from "@codemod.com/workflow";
 
 export async function workflow({ files }: Api) {
+
+  const oldpattern1 = `[data-radix-collection-item]`
+  const newpattern1 = `[data-reka-collection-item]`
+  const oldpattern2 = `--radix-accordion-content-width: $A;`
+  const newpattern2 = `--reka-accordion-content-width: $A;`
+
   await files("*.css")
-    .astGrep("--radix-accordion-content-width: $A;")
-    .replace("--reka-accordion-content-width: $A;");
+    .astGrep(oldpattern1)
+    .replace(newpattern1);
+
+  await files("*.css")
+    .astGrep(oldpattern2)
+    .replace(newpattern2);
   
 
-    await files("*.css")
-    .astGrep("[data-radix-collection-item]")
-    .replace("[data-reka-collection-item]");
 }
